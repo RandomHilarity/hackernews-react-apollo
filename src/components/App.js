a0476e464d6464a78a209281dcc0773bd0325ebe
John Webster <johnrwebs@gmail.com>
2020-11-02 00:35:42 -0700

    added CreateLink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/App.css';
 
-import LinkList from './LinkList'
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <LinkList />
+    <CreateLink />
   );
 }
 
---
+++ src/components/CreateLink.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+function CreateLink(props) {
+  const [state, setState] = React.useState({
+    description: "",
+    url: ""
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.value });
+  };
+  
+  const { description, url } = state;
+
+  return (
+    <div>
+      <div className="flex flex-column mt3">
+        <input
+          id="description"
+          className="mb2"
+          value={ description }
+          onChange={ handleChange("description") }
+          type="text"
+          placeholder="A description for the link"
+        />
+        <input
+          id="url"
+          className="mb2"
+          value={ url }
+          onChange={ handleChange("url") }
+          type="text"
+          placeholder="The URL for the link"
+        />
+      </div>
+      <Mutation mutation={ POST_MUTATION } variables={{ description, url }}>
+        {postMutation => <button onClick={ postMutation }>Submit</button>}
+      </Mutation>
+    </div>
+  )
+};
+
+export default CreateLink;
